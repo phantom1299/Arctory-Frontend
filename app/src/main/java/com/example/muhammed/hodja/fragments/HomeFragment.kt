fc03f7379f57a663878c4484daf0221cdecd66fd
Muhammed Aydemir <muhammed.1299x@gmail.com>
2018-02-17 12:01:10 +0300

    Add Fuel library for Http Requests

--- app/src/main/java/com/example/muhammed/hodja/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/muhammed/hodja/fragments/HomeFragment.kt
@@ -3,17 +3,20 @@ package com.example.muhammed.hodja.fragments
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
 import com.example.muhammed.hodja.R
+import com.github.kittinunf.fuel.Fuel
+import com.github.kittinunf.result.Result
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.fragment_home.*
 
 
 /**
@@ -45,6 +48,22 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
         option.position(pp).title("Başlangıç")
         map!!.addMarker(option)
         map!!.moveCamera(CameraUpdateFactory.newLatLng(pp))
+        Log.d("Result", "Here")
+        getData()
+    }
+
+    fun getData() {
+        Fuel.get(getString(R.string.lessonsUrl), listOf("maxFee" to "1000", "maxDistance" to "500", "studentLocation" to "[50,33]")).responseString { request, response, result ->
+            //do something with response
+            when (result) {
+                is Result.Failure -> {
+                    textView3.text = result.toString()
+                }
+                is Result.Success -> {
+                    textView3.text = result.toString()
+                }
+            }
+        }
     }
 
 }// Required empty public constructor
